819c876cc0b04a4ad8978462086a0c89c2c525cb
OsamiMatsui <matui@sun-e.co.jp>
2016-11-11 20:16:19 +0900

    Initial Commit

---
+++ SunDemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SunDemoApp
+//
+//  Created by 松井 修己 on 2016/11/11.
+//  Copyright © 2016年 松井 修己. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
